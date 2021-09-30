9551501157d6e97b556825a0b222b2afb1ba7754
chenhong1875435468 <1875435468@qq.com>
2021-09-30 21:16:14 +0800

    first commit

---
+++ src/practice/Main.java
@@ -0,0 +1,12 @@
+package practice;
+
+/**
+ * .*.@author.chenhong
+ * .*.@date.2021/9/30
+ * .
+ */
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
