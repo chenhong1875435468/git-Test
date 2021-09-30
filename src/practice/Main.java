bb74a50deaf5b88fea447951e01b5578f691c4d8
chenhong1875435468 <1875435468@qq.com>
2021-09-30 21:28:08 +0800

    hot-fix commit

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("hello git!!!");
         System.out.println("hello git!   ");
         System.out.println("hello git!   ");
+        System.out.println("hello git!   ");
     }
 }
