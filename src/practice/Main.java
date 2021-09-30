62c5a817c6f40e583e3c352711317571db61feea
chenhong1875435468 <1875435468@qq.com>
2021-09-30 21:31:59 +0800

    master commits

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -11,5 +11,6 @@ public class Main {
         System.out.println("hello git!   ");
         System.out.println("hello git!   ");
         System.out.println("hello git!   ");
+        System.out.println("master commits");
     }
 }
